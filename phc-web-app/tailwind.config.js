e461af52c9935d1c20f91370b6a68a57725e7752
MDKR3AP3R <riaanmwilliams@gmail.com>
2025-11-20 17:26:03 +0200

    tailwind fix

--- phc-web-app/tailwind.config.js
+++ phc-web-app/tailwind.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "../*.{js,jsx,ts,tsx,html}",
+    "../app.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
